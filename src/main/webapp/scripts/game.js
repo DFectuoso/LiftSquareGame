045ae3709798b473c85947210bbb2e562b79dba4
DFectuoso <santiago1717@gmail.com>
2010-04-04 00:14:55 -0700

    Adding keystroke sensor

--- src/main/webapp/scripts/game.js
+++ src/main/webapp/scripts/game.js
@@ -1,4 +1,23 @@
 var clearChat = function(){$('#message-textarea')[0].value="";}
+
+var getArrows = function(ev) {
+  arrows=((ev.which)||(ev.keyCode));
+  switch(arrows) {
+    case 37:
+      console.log('left arrow');
+      break;
+    case 38:
+      console.log('up arrow');
+      break;
+    case 39:
+      console.log('right arrow');
+      break;
+    case 40:
+      console.log('down arrow');
+      break;
+  }
+}
+
  
 jQuery('document').ready(function(){
   var submitChatOnEnter = function(e){
@@ -15,4 +34,5 @@ jQuery('document').ready(function(){
   }
 
   $('#message-textarea')[0].onkeypress = submitChatOnEnter;
+  document.onkeydown = getArrows;
 });
